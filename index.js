afb309c4846ef22b52a6957ab00aad8b88cbb231
Olesya <krayanskaya_ov@mail.ru>
2023-08-29 23:20:10 +0300

    add images

--- index.js
+++ index.js
@@ -5,120 +5,140 @@ const dataItems = [
     name: 'item-1',
     price: 20,
     description: 'description-1',
+    img: './images/1.avif',
   },
   {
     id: 2,
     name: 'item-2',
     price: 30,
     description: 'description-2',
+    img: './images/2.avif',
   },
   {
     id: 3,
     name: 'item-3',
     price: 15,
     description: 'description-3',
+    img: './images/3.avif',
   },
   {
     id: 4,
     name: 'item-4',
     price: 22,
     description: 'description-4',
+    img: './images/4.avif',
   },
   {
     id: 5,
     name: 'item-5',
     price: 15,
     description: 'description-5',
+    img: './images/5.avif',
   },
   {
     id: 6,
     name: 'item-6',
     price: 20,
     description: 'description-6',
+    img: './images/6.avif',
   },
   {
     id: 7,
     name: 'item-7',
     price: 15,
     description: 'description-7',
+    img: './images/7.avif',
   },
   {
     id: 8,
     name: 'item-8',
     price: 25,
     description: 'description-8',
+    img: './images/8.avif',
   },
   {
     id: 9,
     name: 'item-9',
     price: 20,
     description: 'description-9',
+    img: './images/9.avif',
   },
   {
     id: 10,
     name: 'item-10',
     price: 15,
     description: 'description-10',
+    img: './images/10.avif',
   },
   {
     id: 11,
     name: 'item-11',
     price: 37,
     description: 'description-11',
+    img: './images/11.avif',
   },
   {
     id: 12,
     name: 'item-12',
     price: 26,
     description: 'description-12',
+    img: './images/12.avif',
   },
   {
     id: 13,
     name: 'item-13',
     price: 45,
     description: 'description-13',
+    img: './images/13.avif',
   },
   {
     id: 14,
     name: 'item-14',
     price: 25,
     description: 'description-14',
+    img: './images/14.avif',
   },
   {
     id: 15,
     name: 'item-15',
     price: 27,
     description: 'description-15',
+    img: './images/15.avif',
   },
   {
     id: 16,
     name: 'item-16',
     price: 22,
     description: 'description-16',
+    img: './images/16.avif',
   },
   {
     id: 17,
     name: 'item-17',
     price: 44,
     description: 'description-17',
+    img: './images/17.avif',
   },
   {
     id: 18,
     name: 'item-18',
     price: 11,
     description: 'description-18',
+    img: './images/18.avif',
   },
   {
     id: 19,
     name: 'item-19',
     price: 23,
     description: 'description-19',
+    img: './images/19.avif',
   },
   {
     id: 20,
     name: 'item-20',
     price: 24,
     description: 'description-20',
+    img: './images/20.avif',
   },
 ];
 
@@ -129,6 +149,7 @@ dataItems.forEach((elem) =>
     name: elem.name,
     price: elem.price,
     description: elem.description,
+    img: elem.img,
   })
 );
 
@@ -233,6 +254,11 @@ function createItem(value, key) {
   name.className = 'item__name';
   name.innerHTML = value.name;
   item.append(name);
+  const image = document.createElement('img');
+  image.className = 'item__img';
+  image.src = `${value.img}`;
+  image.alt = 'img';
+  item.append(image);
   const itemFooter = document.createElement('footer');
   itemFooter.className = 'item__footer';
   item.append(itemFooter);
@@ -300,22 +326,26 @@ function renderCart() {
   itemsInCart.forEach((amount, id, itemsInCart) => {
     const cartItems = document.querySelector('.cart__items');
     const cartItem = document.createElement('div');
-    cartItem.className = 'cart__item item';
+    cartItem.className = 'cart__item item-cart';
     cartItems.append(cartItem);
+    const cartItemImg = document.createElement('img');
+    cartItemImg.className = 'item-cart__img';
+    cartItemImg.src = `${dataMap.get(+id).img}`;
+    cartItem.append(cartItemImg);
     const cartItemName = document.createElement('div');
-    cartItemName.className = 'item__name';
+    cartItemName.className = 'item-cart__name';
     cartItemName.innerHTML = `${dataMap.get(+id).name}`;
     cartItem.append(cartItemName);
     const cartItemPrice = document.createElement('div');
-    cartItemPrice.className = 'item__price';
+    cartItemPrice.className = 'item-cart__price';
     cartItemPrice.innerHTML = `${dataMap.get(+id).price}`;
     cartItem.append(cartItemPrice);
     const cartItemAmount = document.createElement('div');
-    cartItemAmount.className = 'item__amount';
+    cartItemAmount.className = 'item-cart__amount';
     cartItemAmount.innerHTML = amount;
     cartItem.append(cartItemAmount);
     const itemMinus = document.createElement('button');
-    itemMinus.className = 'item__minus';
+    itemMinus.className = 'item-cart__minus';
     itemMinus.innerHTML = '-';
     cartItem.append(itemMinus);
     itemMinus.addEventListener('click', () => {
@@ -333,7 +363,7 @@ function renderCart() {
       totalSum();
     });
     const itemPlus = document.createElement('button');
-    itemPlus.className = 'item__plus';
+    itemPlus.className = 'item-cart__plus';
     itemPlus.innerHTML = '+';
     cartItem.append(itemPlus);
     itemPlus.addEventListener('click', () => {
@@ -344,7 +374,7 @@ function renderCart() {
       totalSum();
     });
     const itemDel = document.createElement('button');
-    itemDel.className = 'item__delete';
+    itemDel.className = 'item-cart__delete';
     itemDel.innerHTML = 'Delete';
     cartItem.append(itemDel);
     itemDel.addEventListener('click', () => {
